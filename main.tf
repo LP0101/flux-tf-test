6401338e1101b763300cb9ee127397e516525a70
Luca Popesco <luca@popesco.io>
2022-10-30 15:27:17 -0400

    Add NS

--- main.tf
+++ main.tf
@@ -1,4 +1,15 @@
+resource "kubernetes_namespace" "hello-world" {
+
+  metadata {
+    name = "hello-world"
+    labels = {
+      name = "hello-world"
+    }
+  }
+}
+
 resource "helm_release" "hello-world" {
+  depends_on = [kubernetes_namespace.hello-world]
   name = "hello-world"
   namespace = "hello-world"
   repository = "https://cloudecho.github.io/charts/"
