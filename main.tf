9a366c8f32b47c15c09cde79c50e97398f023ca4
Luca Popesco <luca@popesco.io>
2022-10-30 13:44:47 -0400

    Remove NS

--- main.tf
+++ main.tf
@@ -1,15 +1,4 @@
-resource "kubernetes_namespace" "hello-world" {
-
-  metadata {
-    name = "hello-world"
-    labels = {
-      name = "hello-world"
-    }
-  }
-}
-
 resource "helm_release" "hello-world" {
-  depends_on = [kubernetes_namespace.hello-world]
   name = "hello-world"
   namespace = "hello-world"
   repository = "https://cloudecho.github.io/charts/"
